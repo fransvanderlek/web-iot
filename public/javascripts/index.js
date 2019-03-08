5f34900b3cb841786f55215ac3f051cf68694e80
fransvanderlek <fransvanderlek@gmail.com>
2019-03-08 11:42:04 +0100

    removed URL parsing

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -71,10 +71,8 @@ $(document).ready(function () {
     console.log('Successfully connect WebSocket');
   }
   ws.onmessage = function (message) {
-    console.log('receive message' + message.data);
+    console.log('receive message zzz' + message.data);
     
-    var url = new URL(location.href);
-    var selectedDeviceId = url.searchParams.get("device-id");
     
     console.log('location : ' + location.href);
     console.log('selectedDeviceId : ' + selectedDeviceId);
